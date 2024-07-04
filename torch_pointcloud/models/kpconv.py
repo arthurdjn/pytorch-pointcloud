f9ee9ec3f6aad9f0fe9f99150d6ad2dafec63c42
Arthur Dujardin <arthurdujardin.dev@gmail.com>
2024-07-04 21:25:59 +0200

    wip kpconv

--- torch_pointcloud/models/kpconv.py
+++ torch_pointcloud/models/kpconv.py
@@ -1,6 +1,86 @@
 # https://github.com/HuguesTHOMAS/KPConv-PyTorch
 # type: ignore
 
+
+# TODO: add load_kernels function
+def load_kernels(radius, num_kpoints, dimension, fixed, lloyd=False):
+
+    # Kernel directory
+    kernel_dir = "kernels/dispositions"
+    if not exists(kernel_dir):
+        makedirs(kernel_dir)
+
+    # To many points switch to Lloyds
+    if num_kpoints > 30:
+        lloyd = True
+
+    # Kernel_file
+    kernel_file = join(kernel_dir, "k_{:03d}_{:s}_{:d}D.ply".format(num_kpoints, fixed, dimension))
+
+    # Check if already done
+    if not exists(kernel_file):
+        if lloyd:
+            # Create kernels
+            kernel_points = spherical_Lloyd(1.0, num_kpoints, dimension=dimension, fixed=fixed, verbose=0)
+
+        else:
+            # Create kernels
+            kernel_points, grad_norms = kernel_point_optimization_debug(
+                1.0, num_kpoints, num_kernels=100, dimension=dimension, fixed=fixed, verbose=0
+            )
+
+            # Find best candidate
+            best_k = np.argmin(grad_norms[-1, :])
+
+            # Save points
+            kernel_points = kernel_points[best_k, :, :]
+
+        write_ply(kernel_file, kernel_points, ["x", "y", "z"])
+
+    else:
+        data = read_ply(kernel_file)
+        kernel_points = np.vstack((data["x"], data["y"], data["z"])).T
+
+    # Random roations for the kernel
+    # N.B. 4D random rotations not supported yet
+    R = np.eye(dimension)
+    theta = np.random.rand() * 2 * np.pi
+    if dimension == 2:
+        if fixed != "vertical":
+            c, s = np.cos(theta), np.sin(theta)
+            R = np.array([[c, -s], [s, c]], dtype=np.float32)
+
+    elif dimension == 3:
+        if fixed != "vertical":
+            c, s = np.cos(theta), np.sin(theta)
+            R = np.array([[c, -s, 0], [s, c, 0], [0, 0, 1]], dtype=np.float32)
+
+        else:
+            phi = (np.random.rand() - 0.5) * np.pi
+
+            # Create the first vector in carthesian coordinates
+            u = np.array([np.cos(theta) * np.cos(phi), np.sin(theta) * np.cos(phi), np.sin(phi)])
+
+            # Choose a random rotation angle
+            alpha = np.random.rand() * 2 * np.pi
+
+            # Create the rotation matrix with this vector and angle
+            R = create_3D_rotations(np.reshape(u, (1, -1)), np.reshape(alpha, (1, -1)))[0]
+
+            R = R.astype(np.float32)
+
+    # Add a small noise
+    kernel_points = kernel_points + np.random.normal(scale=0.01, size=kernel_points.shape)
+
+    # Scale kernels
+    kernel_points = radius * kernel_points
+
+    # Rotate kernels
+    kernel_points = np.matmul(kernel_points, R)
+
+    return kernel_points.astype(np.float32)
+
+
 # ----------------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 # https://github.com/HuguesTHOMAS/KPConv-PyTorch/blob/master/models/architectures.py
