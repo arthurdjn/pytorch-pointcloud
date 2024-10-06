7096b14bc95ece80668098fa612f7da255b2049c
Arthur Dujardin <arthurdujardin.dev@gmail.com>
2024-10-06 21:42:09 +0200

    add cuda support

--- torch_pointcloud/csrc/grid_cluster/grid_cluster.h
+++ torch_pointcloud/csrc/grid_cluster/grid_cluster.h
@@ -6,12 +6,19 @@ at::Tensor grid_cluster_cpu(
     const at::Tensor& lengths,
     const float voxel_size);
 
+at::Tensor grid_cluster_cuda(
+    const at::Tensor& points,
+    const at::Tensor& lengths,
+    const float voxel_size);
+
 at::Tensor grid_cluster(
     const at::Tensor& points,
     const at::Tensor& lengths,
     const float voxel_size) {
   if (points.is_cuda()) {
 #ifdef WITH_CUDA
+    return grid_cluster_cuda(points, lengths, voxel_size);
+#else
     AT_ERROR("CUDA support is not available in this build.");
 #endif
   } else {
