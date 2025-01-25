e0f1517d40d8f357f2a0f62b10430e472885d520
Arthur Dujardin <arthurdujardin.dev@gmail.com>
2025-01-25 11:02:27 +0100

    improve dataset tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Callable
 
 import pytest
 
@@ -6,18 +7,26 @@ DATA_DIR = Path("tests/data")
 
 
 @pytest.fixture
-def data_dir(tmp_path: Path) -> Path:
-    out_dir = tmp_path / "data"
-    for file_path in DATA_DIR.rglob("*"):
-        if not file_path.is_file():
-            continue
+def data_dir_factory(tmp_path: Path) -> Callable[..., Path]:
+    def data_dir(pattern: str = "*") -> Path:
+        out_dir = tmp_path / "data"
+        for file_path in DATA_DIR.rglob(pattern):
+            if not file_path.is_file():
+                continue
 
-        out_path = out_dir / file_path.relative_to(DATA_DIR)
-        out_path.parent.mkdir(parents=True, exist_ok=True)
+            out_path = out_dir / file_path.relative_to(DATA_DIR)
+            out_path.parent.mkdir(parents=True, exist_ok=True)
 
-        if out_path.exists():
-            out_path.unlink()
+            if out_path.exists():
+                out_path.unlink()
 
-        out_path.symlink_to(file_path.absolute())
+            out_path.symlink_to(file_path.absolute())
 
-    return out_dir
+        return out_dir
+
+    return data_dir
+
+
+@pytest.fixture
+def data_dir(data_dir_factory: Callable[..., Path]) -> Path:
+    return data_dir_factory()
