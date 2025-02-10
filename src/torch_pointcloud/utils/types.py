e5daf7ebc529f63e0976a22929aa4820aff17115
Arthur Dujardin <arthurdujardin.dev@gmail.com>
2025-02-10 21:34:47 +0100

    feat: add utility types

--- src/torch_pointcloud/utils/types.py
+++ src/torch_pointcloud/utils/types.py
@@ -1,3 +1,9 @@
 from pathlib import Path
+from typing import Any, Dict, Sequence, TypeVar, Union
 
 PathLike = str | Path
+KeyCollection = Union[str, Sequence[str]]
+
+T = TypeVar("T", bound=Any)
+
+DictStr = Dict[str, T]
