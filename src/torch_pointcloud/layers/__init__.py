8820ff5df5eb3afd5fd1558916ca12c661688301
arthurdjn <48992917+arthurdjn@users.noreply.github.com>
2025-03-03 22:39:52 +0100

    feat: pointnet model (#32)
    * feat: pointnet model
    
    * fix: ignore torch untyped calls

---
+++ src/torch_pointcloud/layers/__init__.py
@@ -0,0 +1,5 @@
+from ._modules import ModuleLike, RegisteredModuleLike, get_module
+from .activations import ActLike, get_act
+from .blocks import conv1d_block, linear_block
+from .heads import create_cls_head, create_pool, create_seg_head
+from .norms import NormLike, get_norm
---
+++ src/torch_pointcloud/layers/heads.py
@@ -0,0 +1,40 @@
+import functools
+import itertools
+from typing import List, Optional, Sequence
+
+import torch
+import torch.nn as nn
+from torch_scatter import scatter
+
+from .activations import ActLike
+from .blocks import LinearBlockOrderLike, linear_block
+from .norms import NormLike
+
+
+def create_pool(reduce: str = "mean", dim: int = 0, dim_size: Optional[int] = None) -> functools.partial:
+    return functools.partial(scatter, dim=dim, dim_size=dim_size, reduce=reduce)
+
+
+def create_cls_head(num_features: int, num_classes: int) -> torch.nn.Module:
+    if num_classes == 0:
+        return nn.Identity()
+
+    return nn.Linear(num_features, num_classes)
+
+
+def create_seg_head(
+    dims: Sequence[int],
+    num_classes: int,
+    act: ActLike = "relu",
+    norm: NormLike = "batch_norm1d",
+    dropout: float = 0.0,
+    order: LinearBlockOrderLike = "land",
+) -> torch.nn.Module:
+    if not dims or num_classes == 0:
+        return nn.Identity()
+
+    blocks: List[nn.Module] = []
+    for in_features, out_features in itertools.pairwise(dims[:-1]):
+        blocks.append(linear_block(in_features, out_features, act=act, norm=norm, dropout=dropout, order=order))
+    blocks.append(nn.Linear(dims[-2], num_classes))
+    return nn.Sequential(*blocks)
