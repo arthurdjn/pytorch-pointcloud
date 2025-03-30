39facfeae9773697ff328f7a251561ec37eb2720
Arthur Dujardin <arthurdujardin.dev@gmail.com>
2025-03-30 19:23:51 +0200

    fix tests and deps

--- tests/utils/test_serialization.py
+++ tests/utils/test_serialization.py
@@ -53,40 +53,26 @@ def test_z_order_trans_encoding(mock_octree: Mock, grid_coords: Tensor, batch: T
     assert torch.equal(args[2], grid_coords[:, 2].long())
 
 
-# @patch("torch_pointcloud.utils.serialization.octree_encode")
-# @patch("torch_pointcloud.utils.serialization.hilbert_encode")
-# def test_hilbert_encoding(
-#     mock_hilbert: Mock,
-#     mock_octree: Mock,
-#     sample_inputs: Tuple[Tensor, Tensor, int],
-# ) -> None:
-#     grid_coords, batch_idx, depth = sample_inputs
+@patch("torch_pointcloud.utils.serialization.hilbert_encode")
+def test_hilbert_encoding(mock_hilbert: Mock, grid_coords: Tensor, batch: Tensor, depth: int) -> None:
+    serialize_coords(grid_coords, batch, depth, order="hilbert")
+    mock_hilbert.assert_called_once_with(ANY, num_dims=3, num_bits=depth)
 
-#     # Test hilbert
-#     serialize_coords(grid_coords, batch_idx, depth, order="hilbert")
-#     mock_hilbert.assert_called_once_with(grid_coords, num_dims=3, num_bits=depth)
-#     mock_octree.assert_not_called()
+    args = mock_hilbert.call_args[0]
+    assert len(args) == 1
+    assert torch.equal(args[0], grid_coords)
 
 
-# @patch("torch_pointcloud.utils.serialization.octree_encode")
-# @patch("torch_pointcloud.utils.serialization.hilbert_encode")
-# def test_hilbert_trans_encoding(
-#     mock_hilbert: Mock,
-#     mock_octree: Mock,
-#     sample_inputs: Tuple[Tensor, Tensor, int],
-# ) -> None:
-#     grid_coords, batch_idx, depth = sample_inputs
+@patch("torch_pointcloud.utils.serialization.hilbert_encode")
+def test_hilbert_trans_encoding(mock_hilbert: Mock, grid_coords: Tensor, batch: Tensor, depth: int) -> None:
+    serialize_coords(grid_coords, batch, depth, order="hilbert-trans")
+    mock_hilbert.assert_called_once_with(ANY, num_dims=3, num_bits=depth)
 
-#     # Test hilbert transposed
-#     serialize_coords(grid_coords, batch_idx, depth, order="hilbert-trans")
-#     mock_hilbert.assert_called_once_with(grid_coords[:, [1, 0, 2]], num_dims=3, num_bits=depth)
-#     mock_octree.assert_not_called()
+    args = mock_hilbert.call_args[0]
+    assert len(args) == 1
+    assert torch.equal(args[0], grid_coords[:, [1, 0, 2]])
 
 
-# def test_invalid_order(sample_inputs: Tuple[Tensor, Tensor, int]) -> None:
-#     grid_coords, batch_idx, depth = sample_inputs
-
-#     with pytest.raises(ValueError, match="Unsupported serialization order"):
-#         serialize_coords(grid_coords, batch_idx, depth, order="invalid")
-#     with pytest.raises(ValueError, match="Unsupported serialization order"):
-#         serialize_coords(grid_coords, batch_idx, depth, order="invalid")
+def test_invalid_order(grid_coords: Tensor, batch: Tensor, depth: int) -> None:
+    with pytest.raises(ValueError, match="Unsupported serialization order"):
+        serialize_coords(grid_coords, batch, depth, order="invalid")
