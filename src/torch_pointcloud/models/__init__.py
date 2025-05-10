2285ec7d33fefba233c8d9c3a9f764aa81386fc5
Arthur Dujardin <arthurdujardin.dev@gmail.com>
2025-05-10 16:52:38 +0200

    add model

--- src/torch_pointcloud/models/__init__.py
+++ src/torch_pointcloud/models/__init__.py
@@ -1,4 +1,4 @@
-from .kpconv import KPConvNetClassification
+from .kpconv import KPConvNetClassification, KPConvNetSegmentation
 from .point_transformer_v2 import PointTransformerV2Classification, PointTransformerV2Segmentation
 from .point_transformer_v3 import PointTransformerV3Classification, PointTransformerV3Segmentation
 from .pointnet import PointNetClassification, PointNetEncoder, PointNetSegmentation
