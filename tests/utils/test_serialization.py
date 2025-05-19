0a9ea73cdc5836b40768e054a87acdac5a19f9e3
arthurdjn <48992917+arthurdjn@users.noreply.github.com>
2025-05-19 22:33:29 +0200

    fix: better warning hints (#70)

--- tests/utils/test_serialization.py
+++ tests/utils/test_serialization.py
@@ -32,7 +32,7 @@ def depth() -> int:
     return 5
 
 
-@pytest.mark.skipif(not _OCNN_AVAILABLE, reason="Ocnn is not installed")
+@pytest.mark.skipif(not _OCNN_AVAILABLE, reason="OCNN is not installed")
 @patch("torch_pointcloud.utils.serialization.octree_encode")
 def test_z_order_encoding(mock_octree: Mock, grid_coords: Tensor, batch: Tensor, depth: int) -> None:
     serialize_coords(grid_coords, batch, depth, order="z")
@@ -44,7 +44,7 @@ def test_z_order_encoding(mock_octree: Mock, grid_coords: Tensor, batch: Tensor,
     assert torch.equal(args[2], grid_coords[:, 2].long())
 
 
-@pytest.mark.skipif(not _OCNN_AVAILABLE, reason="Ocnn is not installed")
+@pytest.mark.skipif(not _OCNN_AVAILABLE, reason="OCNN is not installed")
 @patch("torch_pointcloud.utils.serialization.octree_encode")
 def test_z_order_trans_encoding(mock_octree: Mock, grid_coords: Tensor, batch: Tensor, depth: int) -> None:
     serialize_coords(grid_coords, batch, depth, order="z-trans")
