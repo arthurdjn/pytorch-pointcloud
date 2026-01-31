fix: scaling transform and naming (#105)

src/torch_pointcloud/datasets/s3dis.py
@@ -346,9 +346,9 @@ class S3DIS(PointCloudDataset):
                 room_blocks = [block for block in room_blocks if block is not None]
                 blocks.extend(room_blocks)
 
-            out_path = Path(self.processed_dir, f"{area}.pt")
-            out_path.parent.mkdir(parents=True, exist_ok=True)
-            torch.save(blocks, out_path)
+            dst_path = Path(self.processed_dir, f"{area}.pt")
+            dst_path.parent.mkdir(parents=True, exist_ok=True)
+            torch.save(blocks, dst_path)
 
     def _process_room(
         self,

src/torch_pointcloud/datasets/scannet.py
@@ -603,9 +603,9 @@ class ScanNet(PointCloudDataset):
             if self.pre_transform is not None:
                 data = self.pre_transform(data)
 
-            out_path = Path(self.processed_dir, self.split, f"{scene_id}.pt")
-            out_path.parent.mkdir(parents=True, exist_ok=True)
-            torch.save(data, out_path)
+            dst_path = Path(self.processed_dir, self.split, f"{scene_id}.pt")
+            dst_path.parent.mkdir(parents=True, exist_ok=True)
+            torch.save(data, dst_path)
 
     def _load_processed_data(self) -> Any:
         data_list = []

src/torch_pointcloud/datasets/shapenet.py
@@ -237,8 +237,9 @@ class ShapeNetPart(PointCloudDataset):
         data_list = parallel_map(func, pbar, num_workers=self.num_workers)
         data_list = [data for data in data_list if data is not None]
 
-        Path(self.processed_dir).mkdir(parents=True, exist_ok=True)
-        torch.save(data_list, Path(self.processed_dir, f"{self.split}.pt"))
+        dst_path = Path(self.processed_dir, f"{self.split}.pt")
+        dst_path.parent.mkdir(parents=True, exist_ok=True)
+        torch.save(data_list, dst_path)
 
     def _process_data(self, file_name: str, category_id_to_idx: Dict[str, int]) -> Optional[Dict[str, Any]]:
         file_path = Path(self.raw_dir, file_name.replace("shape_data/", "")).with_suffix(".txt")