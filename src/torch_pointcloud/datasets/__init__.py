d8fb52978ca59167600d24ed99b9ff308555b096
arthurdjn <48992917+arthurdjn@users.noreply.github.com>
2026-01-31 19:41:45 +0100

    feat: modelnet normal resampled dataset (#104)

--- src/torch_pointcloud/datasets/__init__.py
+++ src/torch_pointcloud/datasets/__init__.py
@@ -1,4 +1,4 @@
-from .modelnet import ModelNet10, ModelNet40
+from .modelnet import ModelNet10, ModelNet40, ModelNetNormalResampled
 from .s3dis import S3DIS
 from .scannet import ScanNet
 from .shapenet import ShapeNetPart
