5388d5cc528e06739f6a64e3079818856f1c046d
arthurdjn <48992917+arthurdjn@users.noreply.github.com>
2026-02-01 20:52:42 +0100

    feat: state dict transform utility (#109)

---
+++ src/torch_pointcloud/utils/state_dict.py
@@ -0,0 +1,75 @@
+import re
+from collections import OrderedDict
+from typing import Any, Callable, Dict, Optional
+
+from torch import Tensor
+
+
+def transform_state_dict(
+    state_dict: Dict[str, Any],
+    mapping: Dict[str, str],
+    value_transform: Optional[Callable[[Tensor], Tensor]] = None,
+) -> Dict[str, Any]:
+    """Transform a pytorch module state dict by remapping keys and optionally transforming associated tensors.
+    This function is designed to map the state dict of a pytorch module to a different state dict,
+    facilitating the transfer of weights between different models.
+
+    Args:
+        state_dict: The state dict to transform.
+        mapping: A dictionary mapping the old keys to the new keys.
+        value_transform: A function to transform the values.
+
+    Returns:
+        The transformed state dict.
+
+    Example:
+        ```python
+        import torch
+        from torch_pointcloud.utils.state_dict import transform_state_dict
+
+        state_dict = {
+            "encoder.conv.0.weight": torch.randn(1, 3, 16, 16),
+            "encoder.conv.0.bias": torch.randn(1),
+            "encoder.norm.1.weight": torch.randn(1, 16, 16, 16),
+            "encoder.norm.1.bias": torch.randn(1),
+            "encoder.norm.1.running_mean": torch.randn(16),
+            "encoder.norm.1.running_var": torch.randn(16),
+        }
+        mapping = {
+            "encoder.{module}.{i}.weight": "backbone.{module}.{i+1}.weight",
+            "encoder.{module}.{i}.bias": "backbone.{module}.{i+1}.bias",
+            "encoder.{module}.{i}.running_{stat}": "backbone.{module}.{i+1}.running_{stat}",
+        }
+        state_dict = transform_state_dict(state_dict, mapping)
+        print(state_dict.keys())
+        # {
+        #     "backbone.conv.1.weight": ...,
+        #     "backbone.conv.1.bias": ...,
+        #     "backbone.norm.2.weight": ...,
+        #     "backbone.norm.2.bias": ...,
+        #     "backbone.norm.2.running_mean": ...,
+        #     "backbone.norm.2.running_var": ...,
+        # }
+        ```
+    """
+    value_transform = value_transform or (lambda v: v)
+
+    # build the rules for the key transformation / mapping
+    rules = []
+    for src, dst in mapping.items():
+        pattern = re.escape(src).replace(r"\{", "{").replace(r"\}", "}")
+        pattern = re.sub(r"\{(\w+):int\}", r"(?P<\1>\\d+)", pattern)
+        pattern = re.sub(r"\{(\w+)\}", r"(?P<\1>[^.]+)", pattern)
+        rules.append((re.compile(f"^{pattern}$"), dst))
+
+    def key_transform(key: str) -> str:
+        for pattern, template in rules:
+            if match := pattern.match(key):
+                # cast to int if possible to allow for arithmetic operations
+                # e.g. "param.{i}.weights" -> "param.{i+1}.weights"
+                ctx = {k: (int(v) if v.isdigit() else v) for k, v in match.groupdict().items()}
+                # evaluate the param using the context/template
+                return re.sub(r"\{([^}]+)\}", lambda m: str(eval(m.group(1), {}, ctx)), template)
+        return key
+
+    return OrderedDict((key_transform(k), value_transform(v)) for k, v in state_dict.items())
