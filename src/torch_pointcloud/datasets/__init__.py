189bb7f78238e7d200f0da3a5b9bb2e2924fc146
arthurdjn <48992917+arthurdjn@users.noreply.github.com>
2026-05-09 19:20:23 +0200

    feat(datasets): add parislille3d dataset and tests (#134)
    * add parislille3d datasets and tests
    
    * format

--- src/torch_pointcloud/datasets/__init__.py
+++ src/torch_pointcloud/datasets/__init__.py
@@ -1,4 +1,5 @@
 from .modelnet import ModelNet10, ModelNet40, ModelNetNormalResampled
+from .parislille3d import ParisLille3D
 from .s3dis import S3DIS, S3DISHdf5
 from .scannet import ScanNet, ScanNet20, ScanNet200
 from .scanobjectnn import ScanObjectNN
