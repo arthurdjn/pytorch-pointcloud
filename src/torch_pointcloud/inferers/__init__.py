06624c50f253ef94babffe902a12b3e5b973eb57
arthurdjn <48992917+arthurdjn@users.noreply.github.com>
2026-05-21 21:38:05 +0200

    feat(inferers): sliding knn and tta inferers (#152)

--- src/torch_pointcloud/inferers/__init__.py
+++ src/torch_pointcloud/inferers/__init__.py
@@ -1,2 +1,5 @@
 from .inferer import Inferer
+from .knn_window import KNNWindowInferer, knn_window_inference
 from .simple import SimpleInferer
+from .sliding_window import SlidingWindowInferer, sliding_window_inference
+from .tta import TTAInferer
