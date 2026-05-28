0166fd7f99fffbf08424a05daffb855090b2b75d
Arthur Dujardin <arthurdujardin.dev@gmail.com>
2026-05-28 18:53:56 +0200

    fix imports

--- src/torch_pointcloud/inferers/__init__.py
+++ src/torch_pointcloud/inferers/__init__.py
@@ -1,6 +1,6 @@
-from .cyclic_voxel import CyclicVoxelInferer
 from .inferer import Inferer
 from .knn_window import KNNWindowInferer, knn_window_inference
 from .simple import SimpleInferer
 from .sliding_window import SlidingWindowInferer, sliding_window_inference
 from .tta import TTAInferer
+from .voxel_partition import VoxelPartitionInferer
