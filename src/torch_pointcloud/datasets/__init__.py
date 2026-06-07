2910ff1650897f2013c7b1eebd9133a7d0193ca7
arthurdjn <48992917+arthurdjn@users.noreply.github.com>
2026-06-07 23:26:17 +0200

    feat(datasets): kitti and nuscenes detection datasets (#168)

--- src/torch_pointcloud/datasets/__init__.py
+++ src/torch_pointcloud/datasets/__init__.py
@@ -1,4 +1,6 @@
+from .kitti import KITTI
 from .modelnet import ModelNet10, ModelNet40, ModelNetNormalResampled
+from .nuscenes import NuScenesMini
 from .parislille3d import ParisLille3D
 from .repeat import RepeatDataset
 from .s3dis import S3DIS, S3DISHdf5
